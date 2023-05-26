2815eeca37f7884910bd34d211c9d2db28391442
erfangh-80 <erfangh4871gholami@gmail.com>
2023-05-26 23:49:18 +0330

    create add button component

---
+++ components/molecules/AddButton.tsx
@@ -0,0 +1,9 @@
+import Add from "../atoms/icons/Add";
+
+export const AddButton = () => {
+  return (
+    <div className="flex justify-center drop-shadow-lg border-2 border-dashed mt-5 rounded p-3 cursor-pointer">
+      <Add />
+    </div>
+  );
+};
