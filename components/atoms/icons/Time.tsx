4a4fa85d35b7ad234992a3a04fd7ffea23827cdf
erfangh-80 <erfangh4871gholami@gmail.com>
2023-05-26 23:49:01 +0330

    create icon project

---
+++ components/atoms/icons/Time.tsx
@@ -0,0 +1,19 @@
+export const Time = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.2}
+      stroke="currentColor"
+      className="w-5 h-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+};
+
