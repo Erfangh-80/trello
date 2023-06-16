c5a104ec8bc7ef82eccd5f209d166db25d37780b
erfangh-80 <erfangh4871gholami@gmail.com>
2023-06-16 23:28:27 +0330

    click to outside modal and close

--- components/pages/Dashboard.tsx
+++ components/pages/Dashboard.tsx
@@ -1,14 +1,37 @@
-import { useSelector } from "react-redux";
+import { useEffect, useRef } from "react";
+import { AppDispatch, RootState } from "@/redux/store";
+import { setIsShow } from "@/redux/features/modal/ModalSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 import { Modal } from "../organisms/Modal";
 import Content from "../template/Content";
-import { RootState } from "@/redux/store";
 
 const Dashboard = () => {
   const { isShow } = useSelector((state: RootState) => state.modal);
+  const modalRef = useRef<HTMLDivElement>(null);
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        dispatch(setIsShow());
+      }
+    };
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    // Unbind the event listener on cleanup
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [modalRef]);
 
   return (
     <div className="min-h-screen">
-      {isShow ? <Modal /> : null}
+      {isShow ? <Modal modalRef={modalRef} /> : null}
       <Content />
     </div>
   );
